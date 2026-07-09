267950fb4881a9a5aeaf8f6e55f3e645d74aa40b
unknown <someone@gmail.com\git>
2026-07-10 00:48:00 +0530

    Configure database to support production PostgreSQL dynamically

--- app/database.py
+++ app/database.py
@@ -1,13 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# SQLite database file
-DATABASE_URL = "sqlite:///./expenses.db"
+# Pull database URL from environment, default to SQLite locally
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./expenses.db")
 
-# Create engine (connection)
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Automatically fix postgresql connection scheme if returned as postgres://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+# Create engine
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 # Session to interact with DB
 SessionLocal = sessionmaker(bind=engine)
